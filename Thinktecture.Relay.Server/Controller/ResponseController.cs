a8b0afddfa18635a0e271d2d424660fc3db0868a
Sebastian P.R. Gingter <sebastian@gingter.org>
2017-10-27 10:03:18 +0200

    Fix: large bodies of legacy connectors were forgotten due to wrong length

--- Thinktecture.Relay.Server/Controller/ResponseController.cs
+++ Thinktecture.Relay.Server/Controller/ResponseController.cs
@@ -35,14 +35,14 @@ namespace Thinktecture.Relay.Server.Controller
 
 			if (headerValues == null)
 			{
+				response.ContentLength = response.Body?.Length ?? 0;
+
 				// this is a legacy on premise connector (v1)
 				if (response.Body?.Length >= 0x10000)
 				{
 					_postDataTemporaryStore.SaveResponse(response.RequestId, response.Body);
 					response.Body = null; // free the memory a.s.a.p.
 				}
-
-				response.ContentLength = response.Body?.Length ?? 0;
 			}
 			else
 			{
@@ -56,7 +56,7 @@ namespace Thinktecture.Relay.Server.Controller
 				}
 			}
 
-			_logger?.Trace("Received legacy on-premise response. request-id={0}, message={1}", response.RequestId, response);
+			_logger?.Trace("Received legacy on-premise response. request-id={0}, response-length={1}", response.RequestId, response.ContentLength);
 
 			await _backendCommunication.SendOnPremiseTargetResponse(response.OriginId, response).ConfigureAwait(false);
 
