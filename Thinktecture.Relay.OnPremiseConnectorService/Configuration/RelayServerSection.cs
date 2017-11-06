081af0be24c3b8308cf4479ad5cbf55559efa8be
Sebastian P.R. Gingter <sebastian@gingter.org>
2017-11-06 10:19:47 +0100

    Changed default timeout of on premise connector from 10 to 30 seconds

--- Thinktecture.Relay.OnPremiseConnectorService/Configuration/RelayServerSection.cs
+++ Thinktecture.Relay.OnPremiseConnectorService/Configuration/RelayServerSection.cs
@@ -8,7 +8,7 @@ namespace Thinktecture.Relay.OnPremiseConnectorService.Configuration
 	{
 		private static readonly ConfigurationProperty _baseUrl = new ConfigurationProperty("baseUrl", typeof(string), null, ConfigurationPropertyOptions.IsRequired);
 		private static readonly ConfigurationProperty _ignoreSslErrors = new ConfigurationProperty("ignoreSslErrors", typeof(bool), false);
-		private static readonly ConfigurationProperty _requestTimeout = new ConfigurationProperty("timeout", typeof(TimeSpan), "00:00:10", new TimeSpanConverter(), new PositiveTimeSpanValidator(), ConfigurationPropertyOptions.None);
+		private static readonly ConfigurationProperty _requestTimeout = new ConfigurationProperty("timeout", typeof(TimeSpan), "00:00:30", new TimeSpanConverter(), new PositiveTimeSpanValidator(), ConfigurationPropertyOptions.None);
 		private static readonly ConfigurationProperty _security = new ConfigurationProperty("security", typeof(SecurityElement), null, ConfigurationPropertyOptions.IsRequired);
 		private static readonly ConfigurationProperty _onPremiseTargets = new ConfigurationProperty("onPremiseTargets", typeof(OnPremiseTargetCollection), null);
 
