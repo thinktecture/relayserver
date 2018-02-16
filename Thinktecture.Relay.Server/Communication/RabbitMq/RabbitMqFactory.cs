4be81c06808253aebfed01f8390d9f3d980b4dd5
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2018-02-16 18:39:28 +0100

    Connection string needs to be a valid ampq uri

--- Thinktecture.Relay.Server/Communication/RabbitMq/RabbitMqFactory.cs
+++ Thinktecture.Relay.Server/Communication/RabbitMq/RabbitMqFactory.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Configuration;
 using RabbitMQ.Client;
 using Serilog;
@@ -28,9 +29,48 @@ namespace Thinktecture.Relay.Server.Communication.RabbitMq
 				throw new ConfigurationErrorsException("Could not find a connection string for RabbitMQ. Please add a connection string in the <connectionStrings> section of the application's configuration file. For example: <add name=\"RabbitMQ\" connectionString=\"host=localhost\" />");
 			}
 
-			_logger?.Verbose("Creating RabbitMQ Bus. connection-string={RabbitConnectionString}", _configuration.RabbitMqConnectionString);
+			try
+			{
+				_logger?.Verbose("Creating RabbitMQ Bus. connection-string={RabbitConnectionString}", _configuration.RabbitMqConnectionString);
 
-			return _factory.CreateConnection(connectionString);
+				_factory.Uri = new Uri(connectionString);
+
+				var endpoints = BuildEndpointListFromConfig();
+				return endpoints.Count > 0
+					? _factory.CreateConnection(endpoints)
+					: _factory.CreateConnection();
+			}
+			catch (Exception ex)
+			{
+				_logger?.Fatal(ex, "Cannot connect to RabbitMQ using the provided configuration.");
+				throw;
+			}
+		}
+
+		private List<AmqpTcpEndpoint> BuildEndpointListFromConfig()
+		{
+			var result = new List<AmqpTcpEndpoint>();
+
+			foreach (var host in _configuration.RabbitHosts)
+			{
+				var parts = host.Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+				if (parts.Length == 1)
+				{
+					result.Add(new AmqpTcpEndpoint(host));
+					_logger?.Information("Adding RabbitMQ host {RabbitMQHost} to available list.", host);
+				}
+				else if (parts.Length == 2 && Int32.TryParse(parts[1], out var port))
+				{
+					result.Add(new AmqpTcpEndpoint(parts[0], port));
+					_logger?.Information("Adding RabbitMQ host {RabbitMQHost} to available list.", host);
+				}
+				else
+				{
+					_logger?.Error("Provided RabbitMQ host {RabbitMQHost} cannot be build into an AmqpTcpEndpoint. Skipping this entry.", host);
+				}
+			}
+
+			return result;
 		}
 	}
 }
