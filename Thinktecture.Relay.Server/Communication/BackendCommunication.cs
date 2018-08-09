949cbb37080cfc772b4b63724240073a1d134135
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2018-08-09 15:07:18 +0200

    Callback timeout should be used before logging

--- Thinktecture.Relay.Server/Communication/BackendCommunication.cs
+++ Thinktecture.Relay.Server/Communication/BackendCommunication.cs
@@ -123,11 +123,13 @@ namespace Thinktecture.Relay.Server.Communication
 		{
 			CheckDisposed();
 
+			requestTimeout = requestTimeout ?? _configuration.OnPremiseConnectorCallbackTimeout;
+
 			_logger?.Debug("Waiting for response. request-id={RequestId}, timeout={Timeout}", requestId, requestTimeout);
 
 			var onPremiseConnectorCallback = _requestCompletedCallbacks[requestId] = _requestCallbackFactory.Create(requestId);
 
-			return GetOnPremiseTargetResponseAsync(onPremiseConnectorCallback, requestTimeout ?? _configuration.OnPremiseConnectorCallbackTimeout, _cancellationToken);
+			return GetOnPremiseTargetResponseAsync(onPremiseConnectorCallback, requestTimeout, _cancellationToken);
 		}
 
 		public void SendOnPremiseConnectorRequest(Guid linkId, IOnPremiseConnectorRequest request)
