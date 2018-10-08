8e0d866bbd2dfd09f1df8616cbdc9739efbc94de
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2018-10-08 15:22:29 +0200

    Completed and unified code documentation for Thinktecture.Relay

--- Thinktecture.Relay/Server/AcknowledgmentMode.cs
+++ Thinktecture.Relay/Server/AcknowledgmentMode.cs
@@ -1,21 +1,24 @@
 namespace Thinktecture.Relay.Server
 {
+	/// <summary>
+	/// Defines the mode in which messages will be acknowledged.
+	/// </summary>
 	public enum AcknowledgmentMode
 	{
 		/// <summary>
 		/// Requests in the message queue will be automatically acknowledged
-		/// by the on-premise before contacting the local target
+		/// by the on-premise before contacting the local target.
 		/// </summary>
 		Default,
 
 		/// <summary>
 		/// Requests in the message queue will be automatically acknowledged
-		/// by the RelayServer before sending to the on-premise
+		/// by the RelayServer before sending to the on-premise.
 		/// </summary>
 		Auto,
 
 		/// <summary>
-		/// Requests in the message queue need to be acknowledged manually
+		/// Requests in the message queue need to be acknowledged manually.
 		/// </summary>
 		Manual,
 	}
