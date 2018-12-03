988b8a75b4ab139ae27ca686d1e5497c72a33b4a
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2018-12-03 14:48:29 +0100

    Added some logging to checker

--- Thinktecture.Relay.OnPremiseConnector/SignalR/AutomaticDisconnectChecker.cs
+++ Thinktecture.Relay.OnPremiseConnector/SignalR/AutomaticDisconnectChecker.cs
@@ -16,10 +16,13 @@ namespace Thinktecture.Relay.OnPremiseConnector.SignalR
 		{
 			if (connection.AbsoluteConnectionLifetime.HasValue && connection.ConnectedSince.HasValue)
 			{
+				_logger?.Debug("Checking if connection reached maximum absolute lifetime. relay-server-connection-instance-id={RelayServerConnectionInstanceId}", connection.RelayServerConnectionInstanceId);
+
 				var endOfMaximumConnectionTime = connection.ConnectedSince + connection.AbsoluteConnectionLifetime;
 
 				if (DateTime.UtcNow > endOfMaximumConnectionTime)
 				{
+					_logger?.Information("Connection reached maximum absolute lifetime: Disconnecting. relay-server-connection-instance-id={RelayServerConnectionInstanceId}", connection.RelayServerConnectionInstanceId);
 					connection.Disconnect();
 					return true;
 				}
@@ -27,10 +30,14 @@ namespace Thinktecture.Relay.OnPremiseConnector.SignalR
 
 			if (connection.SlidingConnectionLifetime.HasValue && connection.LastActivity.HasValue)
 			{
+				_logger?.Debug("Checking if connection reached maximum sliding lifetime. relay-server-connection-instance-id={RelayServerConnectionInstanceId}", connection.RelayServerConnectionInstanceId);
+
 				var endOfSlidingConnectionTime = connection.LastActivity + connection.SlidingConnectionLifetime;
 
 				if (DateTime.UtcNow > endOfSlidingConnectionTime)
 				{
+					_logger?.Information("Connection reached maximum sliding lifetime: Disconnecting. relay-server-connection-instance-id={RelayServerConnectionInstanceId}", connection.RelayServerConnectionInstanceId);
+
 					connection.Disconnect();
 					return true;
 				}
