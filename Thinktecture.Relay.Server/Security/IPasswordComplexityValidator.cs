f104faf7e744dc7a4703d8c05d5d1206f9c916b2
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2019-02-01 12:05:48 +0100

    Made password complexity validator exchangable

--- Thinktecture.Relay.Server/Security/IPasswordComplexityValidator.cs
+++
@@ -1,7 +0,0 @@
-namespace Thinktecture.Relay.Server.Security
-{
-	public interface IPasswordComplexityValidator
-	{
-		bool ValidatePassword(string userName, string password, out string errorMessage);
-	}
-}
