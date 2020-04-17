ff0a7f83769105746b6637c128f5239c95669754
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2020-04-17 12:57:10 +0200

    feat(connector-abstractions): add more interfaces

---
+++ src/Thinktecture.Relay.Connector.Abstractions/IRelayTarget.cs
@@ -0,0 +1,25 @@
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Thinktecture.Relay.Abstractions;
+
+namespace Thinktecture.Relay.Connector.Abstractions
+{
+	/// <summary>
+	/// An implementation of a target requesting the necessary information for a connector.
+	/// </summary>
+	/// <typeparam name="TRequest">The type of request.</typeparam>
+	/// <typeparam name="TResponse">The type of response.</typeparam>
+	public interface IRelayTarget<in TRequest, TResponse>
+		where TRequest : IRelayClientRequest
+		where TResponse : IRelayTargetResponse, new()
+	{
+		/// <summary>
+		/// Called when the target should be requested.
+		/// </summary>
+		/// <param name="request">The metadata of the request.</param>
+		/// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is <see cref="CancellationToken.None"/>.</param>
+		/// <returns>A <see cref="Task"/> representing the asynchronous operation, which wraps the response.</returns>
+		Task<TResponse> HandleAsync(TRequest request, CancellationToken cancellationToken = default);
+	}
+}
