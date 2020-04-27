e4b4c12c39b36451d734a5dd383b832669328aba
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2020-04-27 16:09:17 +0200

    feat(database): add initial relayserver host with db migrations

---
+++ src/hosts/Thinktecture.Relay.Server.Docker/Startup.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Thinktecture.Relay.Server.Persistence.EntityFrameworkCore.DbContexts;
+
+namespace Thinktecture.Relay.Server.Docker
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllers();
+
+			services.AddDbContext<RelayServerConfigurationDbContext>(options =>
+			{
+				options.UseNpgsql(Configuration.GetConnectionString("PostgreSql"), builder =>
+				{
+					builder.MigrationsAssembly("Thinktecture.Relay.Server.EntityFramework.PostgreSql");
+				});
+			});
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+			app.UseAuthentication();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+	}
+}
