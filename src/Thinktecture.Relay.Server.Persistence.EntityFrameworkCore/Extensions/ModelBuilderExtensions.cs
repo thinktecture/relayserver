ec1a92003fe3d1d43b56fac86236c27882bb5cdd
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2020-04-27 15:58:17 +0200

    feat(persistence): add ef core db context assembly

---
+++ src/Thinktecture.Relay.Server.Persistence.EntityFrameworkCore/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,71 @@
+using Thinktecture.Relay.Server.Persistence.EntityFrameworkCore.DbContexts;
+using Thinktecture.Relay.Server.Persistence.EntityFrameworkCore.Entities;
+
+// ReSharper disable once CheckNamespace; Justification: Extension Methods on ModelBuilder
+namespace Microsoft.EntityFrameworkCore
+{
+	/// <summary>
+	/// Extensions methods for the <see cref="ModelBuilder"/>.
+	/// </summary>
+	public static class ModelBuilderExtensions
+	{
+		/// <summary>
+		/// Configures the <see cref="RelayServerConfigurationDbContext"/> entities.
+		/// </summary>
+		/// <param name="modelBuilder">The <see cref="ModelBuilder"/> to configure.</param>
+		/// <returns>The configured instance of the <see cref="ModelBuilder"/>.</returns>
+		public static ModelBuilder ConfigureConfigurationEntities(this ModelBuilder modelBuilder)
+		{
+			return modelBuilder
+				.ConfigureTenant()
+				.ConfigureClientSecret();
+		}
+
+		/// <summary>
+		/// Configures the <see cref="Tenant"/> entity.
+		/// </summary>
+		/// <param name="modelBuilder">The <see cref="ModelBuilder"/> to configure.</param>
+		/// <returns>The configured instance of the <see cref="ModelBuilder"/>.</returns>
+		public static ModelBuilder ConfigureTenant(this ModelBuilder modelBuilder)
+		{
+			return modelBuilder.Entity<Tenant>(tenant =>
+			{
+				tenant
+					.HasKey(t => t.Id);
+
+				tenant
+					.Property(t => t.Name)
+					.IsRequired();
+
+				tenant
+					.HasIndex(t => t.Name)
+					.IsUnique();
+
+				tenant.HasMany(t => t.ClientSecrets)
+					.WithOne(cs => cs.Tenant)
+					.HasForeignKey(cs => cs.TenantId)
+					.IsRequired()
+					.OnDelete(DeleteBehavior.Cascade);
+			});
+		}
+
+		/// <summary>
+		/// Configures the <see cref="ClientSecret"/> entity.
+		/// </summary>
+		/// <param name="modelBuilder">The <see cref="ModelBuilder"/> to configure.</param>
+		/// <returns>The configured instance of the <see cref="ModelBuilder"/>.</returns>
+		public static ModelBuilder ConfigureClientSecret(this ModelBuilder modelBuilder)
+		{
+			return modelBuilder.Entity<ClientSecret>(clientSecret =>
+			{
+				clientSecret
+					.HasKey(t => t.Id);
+
+				clientSecret
+					.Property(t => t.Value)
+					.HasMaxLength(4000)
+					.IsRequired();
+			});
+		}
+	}
+}
