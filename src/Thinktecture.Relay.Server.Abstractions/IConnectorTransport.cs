fae4c49e854520f9a3ca510ae36c126930aca28f
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2020-04-28 15:14:05 +0200

    feat(server-abstractions): add request method

--- src/Thinktecture.Relay.Server.Abstractions/IConnectorTransport.cs
+++ src/Thinktecture.Relay.Server.Abstractions/IConnectorTransport.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Thinktecture.Relay.Abstractions;
 
 namespace Thinktecture.Relay.Server.Abstractions
@@ -6,8 +7,15 @@ namespace Thinktecture.Relay.Server.Abstractions
 	/// An implementation of a transport from and to the connector.
 	/// </summary>
 	/// <typeparam name="TRequest">The type of request.</typeparam>
-	public interface IConnectorTransport<TRequest>
+	public interface IConnectorTransport<in TRequest, TResponse>
 		where TRequest : IRelayClientRequest
+		where TResponse : IRelayTargetResponse
 	{
+		/// <summary>
+		/// Request the response from a target
+		/// </summary>
+		/// <param name="request"></param>
+		/// <returns></returns>
+		Task<TResponse> RequestTargetAsync(TRequest request);
 	}
 }
