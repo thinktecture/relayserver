e638ff18df3f0285687c8aa15c91b374ccc5ce3e
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2020-04-30 19:34:41 +0200

    chore: unify ports and connectionstrings for local and docker starts

--- src/hosts/Thinktecture.Relay.IdentityServer.Docker/run-container.ps1
+++ src/hosts/Thinktecture.Relay.IdentityServer.Docker/run-container.ps1
@@ -3,6 +3,6 @@ docker rm -f relay_identityserver
 docker run `
   --name relay_identityserver `
   --link relay_persistence_postgresql:database `
-  -p 5001:80 `
+  -p 5002:80 `
   -d `
   relay_identityserver
--- src/hosts/Thinktecture.Relay.ManagementApi.Docker/run-container.ps1
+++ src/hosts/Thinktecture.Relay.ManagementApi.Docker/run-container.ps1
@@ -3,6 +3,6 @@ docker rm -f relay_management
 docker run `
   --name relay_management `
   --link relay_persistence_postgresql:database `
-  -p 5002:80 `
+  -p 5004:80 `
   -d `
   relay_management
