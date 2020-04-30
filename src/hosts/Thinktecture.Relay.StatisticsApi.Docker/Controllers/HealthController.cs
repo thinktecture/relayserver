f1596e7ce979d552650574cab1df68c80fd7492e
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2020-04-30 14:42:45 +0200

    feat(statistics): add initial statistics api host

---
+++ src/hosts/Thinktecture.Relay.StatisticsApi.Docker/Controllers/HealthController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Thinktecture.Relay.StatisticsApi.Docker.Controllers
+{
+	[AllowAnonymous]
+	[Route("{controller}/{action}")]
+	public class HealthController : Controller
+	{
+		[HttpGet]
+		public IActionResult Ready()
+		{
+			return Ok();
+		}
+
+		[HttpGet]
+		public IActionResult Check()
+		{
+			return Ok();
+		}
+	}
+}
---
+++ src/hosts/Thinktecture.Relay.StatisticsApi.Docker/Program.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Thinktecture.Relay.Server.Persistence.EntityFrameworkCore.DbContexts;
+
+namespace Thinktecture.Relay.StatisticsApi.Docker
+{
+	public class Program
+	{
+		public static async Task<int> Main(string[] args)
+		{
+			try
+			{
+				var host = CreateHostBuilder(args).Build();
+
+				await host.RunAsync();
+			}
+			catch (Exception ex)
+			{
+				Log.Fatal(ex, "A fatal error cause service crash.");
+				return 1;
+			}
+			finally
+			{
+				Log.CloseAndFlush();
+			}
+
+			return 0;
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host
+				.CreateDefaultBuilder(args)
+				.UseSerilog((context, loggerConfiguration) =>
+				{
+					loggerConfiguration
+						.MinimumLevel.Information()
+						.Enrich.FromLogContext()
+						.Enrich.WithProperty("Application", "StatisticsApi")
+						.ReadFrom.Configuration(context.Configuration)
+						.WriteTo.Console();
+				})
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				});
+	}
+}
