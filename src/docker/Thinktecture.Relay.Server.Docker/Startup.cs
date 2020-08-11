e1acf80032852417890749b8233498694acbfe8e
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2020-08-11 09:18:22 +0200

    feat(docker): add signalr connector transport to di

--- src/docker/Thinktecture.Relay.Server.Docker/Startup.cs
+++ src/docker/Thinktecture.Relay.Server.Docker/Startup.cs
@@ -21,7 +21,9 @@ namespace Thinktecture.Relay.Server.Docker
 		{
 			services.AddControllers();
 			services.AddRelayServerConfigurationDbContext(Configuration.GetConnectionString("PostgreSql"));
-			services.AddRelayServer();
+			services.AddRelayServer()
+				.AddInMemoryServerRouting()
+				.AddSignalRConnectorTransport();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
