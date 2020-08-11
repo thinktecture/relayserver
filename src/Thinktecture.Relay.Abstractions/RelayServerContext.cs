d7c04fe22b578527d4190657032a14ac4c253568
Sebastian P.R. Gingter <sebastian.gingter@thinktecture.com>
2020-08-11 16:42:54 +0200

    docs(abstractions): change xml comments
    Co-authored-by: Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>

--- src/Thinktecture.Relay.Abstractions/RelayServerContext.cs
+++ src/Thinktecture.Relay.Abstractions/RelayServerContext.cs
@@ -3,13 +3,14 @@ using System;
 namespace Thinktecture.Relay
 {
 	/// <summary>
-	/// Provides contextual information for this RelayServer instance.
+	/// Provides contextual information.
 	/// </summary>
 	public class RelayServerContext
 	{
 		/// <summary>
-		/// The unique id of this instance of RelayServer.
+		/// The unique id of this instance.
 		/// </summary>
+		/// <remarks>This changes during every startup.</remarks>
 		public Guid OriginId { get; } = Guid.NewGuid();
 	}
 }
