2f4a902062e132138a6c3164e22d57c347683551
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2020-08-12 09:17:11 +0200

    feat(server): add in-memory body store

---
+++ src/Thinktecture.Relay.Server/Transport/InMemoryBodyStore.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thinktecture.Relay.Server.Transport
+{
+	/// <inheritdoc />
+	public class InMemoryBodyStore : IBodyStore
+	{
+		private readonly ConcurrentDictionary<Guid, byte[]> _requestStore = new ConcurrentDictionary<Guid, byte[]>();
+		private readonly ConcurrentDictionary<Guid, byte[]> _responseStore = new ConcurrentDictionary<Guid, byte[]>();
+
+		/// <inheritdoc />
+		public async Task<long> StoreRequestBodyAsync(Guid requestId, Stream bodyStream, CancellationToken cancellationToken = default)
+		{
+			return await StoreDataAsync(requestId, bodyStream, _requestStore, cancellationToken);
+		}
+
+		/// <inheritdoc />
+		public async Task<long> StoreResponseBodyAsync(Guid requestId, Stream bodyStream, CancellationToken cancellationToken = default)
+		{
+			return await StoreDataAsync(requestId, bodyStream, _responseStore, cancellationToken);
+		}
+
+		/// <inheritdoc />
+		public Task<Stream> OpenRequestBodyAsync(Guid requestId, CancellationToken cancellationToken = default)
+		{
+			return Task.FromResult(GetStream(requestId, _requestStore));
+		}
+
+		/// <inheritdoc />
+		public Task<Stream> OpenResponseBodyAsync(Guid requestId, CancellationToken cancellationToken = default)
+		{
+			return Task.FromResult(GetStream(requestId, _responseStore));
+		}
+
+		/// <inheritdoc />
+		public Task RemoveRequestBodyAsync(Guid requestId, CancellationToken cancellationToken = default)
+		{
+			_requestStore.Remove(requestId, out _);
+			return Task.CompletedTask;
+		}
+
+		/// <inheritdoc />
+		public Task RemoveResponseBodyAsync(Guid requestId, CancellationToken cancellationToken = default)
+		{
+			_responseStore.Remove(requestId, out _);
+			return Task.CompletedTask;
+		}
+
+		private async Task<long> StoreDataAsync(Guid id, Stream stream, ConcurrentDictionary<Guid, byte[]> store, CancellationToken cancellationToken)
+		{
+			await using var ms = new MemoryStream();
+			if (stream.CanSeek)
+			{
+				stream.Position = 0;
+			}
+
+			await stream.CopyToAsync(ms, cancellationToken);
+
+			var data = ms.ToArray();
+
+			store.TryAdd(id, data);
+			return data.LongLength;
+		}
+
+		private Stream GetStream(Guid id, ConcurrentDictionary<Guid, byte[]> store)
+		{
+			return new MemoryStream(store[id]);
+		}
+	}
+}
