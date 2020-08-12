8f6ed84830cc442f60e39e8e10e2fa79ff0aaa25
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2020-08-12 13:48:35 +0200

    feat(server): add stream extensions

--- src/Thinktecture.Relay.Abstractions/StreamExtensions.cs
+++ src/Thinktecture.Relay.Abstractions/StreamExtensions.cs
@@ -10,6 +10,22 @@ namespace Thinktecture.Relay
 	public static class StreamExtensions
 	{
 		/// <summary>
+		/// Sets the position of the stream to zero, if the stream supports seeking.
+		/// </summary>
+		/// <param name="stream">The <see cref="Stream"/> that will be rewind.</param>
+		/// <returns>True, if rewinding was successful; otherwise, false</returns>
+		public static bool TryRewind(this Stream stream)
+		{
+			if (!stream.CanSeek)
+			{
+				return false;
+			}
+
+			stream.Position = 0;
+			return true;
+		}
+
+		/// <summary>
 		/// Asynchronously reads the bytes from the current stream and writes them to a <see cref="MemoryStream"/>, using a cancellation token.
 		/// </summary>
 		/// <param name="stream">The <see cref="Stream"/> from which the contents will be copied.</param>
@@ -17,13 +33,9 @@ namespace Thinktecture.Relay
 		/// <returns>A <see cref="Task"/> representing the asynchronous operation, which wraps the <see cref="MemoryStream"/>.</returns>
 		public static async Task<MemoryStream> CopyToMemoryStreamAsync(this Stream stream, CancellationToken cancellationToken = default)
 		{
+			stream.TryRewind();
+
 			var memoryStream = new MemoryStream();
-
-			if (stream.CanSeek)
-			{
-				stream.Position = 0;
-			}
-
 			await stream.CopyToAsync(memoryStream, 80 * 1024, cancellationToken);
 
 			memoryStream.Position = 0;
