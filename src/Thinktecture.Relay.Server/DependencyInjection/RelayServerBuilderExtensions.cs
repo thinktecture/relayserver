0e03ef3b7b43918bf2db6d579c73c33593925f0d
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2020-08-12 13:49:34 +0200

    feat(server): add file system body store

--- src/Thinktecture.Relay.Server/DependencyInjection/RelayServerBuilderExtensions.cs
+++ src/Thinktecture.Relay.Server/DependencyInjection/RelayServerBuilderExtensions.cs
@@ -1,3 +1,6 @@
+using System;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
 using Thinktecture.Relay.Server;
 using Thinktecture.Relay.Server.DependencyInjection;
 using Thinktecture.Relay.Server.Transport;
@@ -45,6 +48,27 @@ namespace Microsoft.Extensions.DependencyInjection
 			where TResponse : IRelayTargetResponse
 		{
 			builder.Services.AddSingleton<IBodyStore, InMemoryBodyStore>();
+
+			return builder;
+		}
+
+		/// <summary>
+		/// Adds the file-based body store.
+		/// </summary>
+		/// <typeparam name="TRequest"></typeparam>
+		/// <typeparam name="TResponse"></typeparam>
+		/// <param name="builder">The <see cref="IRelayServerBuilder{TRequest,TResponse}"/> instance.</param>
+		/// <param name="configure">A configure callback for setting the <see cref="FileBodyStoreOptions"/>.</param>
+		/// <returns>The <see cref="IRelayServerBuilder{TRequest,TResponse}"/> instance.</returns>
+		/// <remarks>Use a shared location between all server instances.</remarks>
+		public static IRelayServerBuilder<TRequest, TResponse> AddFileBodyStore<TRequest, TResponse>(
+			this IRelayServerBuilder<TRequest, TResponse> builder, Action<FileBodyStoreOptions> configure)
+			where TRequest : IRelayClientRequest
+			where TResponse : IRelayTargetResponse
+		{
+			builder.Services.Configure(configure);
+			builder.Services.AddTransient<IValidateOptions<FileBodyStoreOptions>, FileBodyStoreOptionsValidator>();
+			builder.Services.AddSingleton<IBodyStore, FileBodyStore>();
 
 			return builder;
 		}
