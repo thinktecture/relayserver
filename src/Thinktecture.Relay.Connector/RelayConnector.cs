0af3fada16f1f6d97d009f5537f06f0b44361081
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2020-08-14 12:08:48 +0200

    feat(connector): implement relay connector

--- src/Thinktecture.Relay.Connector/RelayConnector.cs
+++ src/Thinktecture.Relay.Connector/RelayConnector.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace Thinktecture.Relay.Connector
 {
 	/// <summary>
@@ -5,5 +9,31 @@ namespace Thinktecture.Relay.Connector
 	/// </summary>
 	public class RelayConnector
 	{
+		private readonly IConnectorConnection _connection;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="connection"></param>
+		public RelayConnector(IConnectorConnection connection)
+		{
+			_connection = connection ?? throw new ArgumentNullException(nameof(connection));
+		}
+
+		/// <summary>
+		/// Opens a connection to the server.
+		/// </summary>
+		/// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is <see cref="CancellationToken.None"/>.</param>
+		/// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+		public Task ConnectAsync(CancellationToken cancellationToken = default)
+			=> _connection.ConnectAsync(cancellationToken);
+
+		/// <summary>
+		/// Closes the connection to the server.
+		/// </summary>
+		/// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is <see cref="CancellationToken.None"/>.</param>
+		/// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+		Task DisconnectAsync(CancellationToken cancellationToken = default)
+			=> _connection.DisconnectAsync(cancellationToken);
 	}
 }
