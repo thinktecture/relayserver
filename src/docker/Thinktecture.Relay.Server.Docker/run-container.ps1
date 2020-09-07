bfe677f769127d188eba37af1301f1c66d69ecba
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2020-09-07 08:26:36 +0200

    fix(environment): harmonize docker and app settings

--- src/docker/Thinktecture.Relay.Server.Docker/run-container.ps1
+++ src/docker/Thinktecture.Relay.Server.Docker/run-container.ps1
@@ -3,6 +3,7 @@ docker rm -f relay_server
 docker run `
   --name relay_server `
   --link relay_persistence_postgresql:database `
+  --link relay_transport_rabbitmq:transport `
   -p 5000:80 `
   -d `
   relay_server
