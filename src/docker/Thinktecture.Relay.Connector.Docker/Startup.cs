1e544a7eab932cf496524b4b7b5aa40389e826d8
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2020-09-07 11:31:16 +0200

    feat(connector): add start/stop logging

--- src/docker/Thinktecture.Relay.Connector.Docker/Startup.cs
+++ src/docker/Thinktecture.Relay.Connector.Docker/Startup.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Thinktecture.Relay.Connector.RelayTargets;
 
 namespace Thinktecture.Relay.Connector.Docker
@@ -26,26 +27,30 @@ namespace Thinktecture.Relay.Connector.Docker
 
 	internal class ConnectorService : IHostedService, IDisposable
 	{
+		private readonly ILogger<ConnectorService> _logger;
 		private readonly RelayConnector _connector;
 		private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
 
-		public ConnectorService(RelayConnector connector)
+		public ConnectorService(ILogger<ConnectorService> logger, RelayConnector connector)
 		{
+			_logger = logger ?? throw new ArgumentNullException(nameof(logger));
 			_connector = connector ?? throw new ArgumentNullException(nameof(connector));
 		}
 
 		public Task StartAsync(CancellationToken cancellationToken)
 		{
+			_logger.LogInformation("Starting connector");
 			_connector.ConnectAsync(_cancellationTokenSource.Token);
 			return Task.CompletedTask;
 		}
 
 		public async Task StopAsync(CancellationToken cancellationToken)
 		{
+			_logger.LogInformation("Gracefully stopping connector");
 			_cancellationTokenSource.Cancel();
 			await _connector.DisconnectAsync(cancellationToken);
 		}
 
-		public void Dispose() => _cancellationTokenSource.Cancel();
+		public void Dispose() => _cancellationTokenSource.Dispose();
 	}
 }
