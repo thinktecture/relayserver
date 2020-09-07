1958200fd527702c679ff47e950d5c56670a327f
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2020-09-07 08:26:36 +0200

    fix(environment): harmonize docker and app settings

--- src/docker/run-dependencies.ps1
+++ src/docker/run-dependencies.ps1
@@ -12,12 +12,15 @@ docker run `
   -d `
   postgres
 
-docker rm -f relay_rabbitmq
+docker volume create relay_transport_rabbitmq
+
+docker rm -f relay_transport_rabbitmq
 docker run `
-  --name relay_rabbitmq `
-  --hostname relay-rabbitmq `
+  --name relay_transport_rabbitmq `
+  --hostname relay-transport-rabbitmq `
   -e RABBITMQ_DEFAULT_USER=relayserver `
   -e RABBITMQ_DEFAULT_PASS=<Strong!Passw0rd> `
+  -v relay_transport_rabbitmq:/var/lib/rabbitmq `
   -p 5672:5672 `
   -p 15672:15672 `
   -d `
