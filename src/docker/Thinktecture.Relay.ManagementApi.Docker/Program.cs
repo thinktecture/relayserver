5b44c1c9ffb9f5f01cc7f625f74c82e639532da6
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2020-09-08 12:11:57 +0200

    chore(management): code cleanup

--- src/docker/Thinktecture.Relay.ManagementApi.Docker/Program.cs
+++ src/docker/Thinktecture.Relay.ManagementApi.Docker/Program.cs
@@ -1,12 +1,8 @@
 using System;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Thinktecture.Relay.Docker;
-using Thinktecture.Relay.Server.Persistence.EntityFrameworkCore.DbContexts;
 
 namespace Thinktecture.Relay.ManagementApi.Docker
 {
