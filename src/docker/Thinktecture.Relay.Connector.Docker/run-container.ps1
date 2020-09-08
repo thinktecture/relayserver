71621bc860d128bc2861a89173fb4c746f753676
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2020-09-08 15:52:56 +0200

    docker: add required links to identity server

--- src/docker/Thinktecture.Relay.Connector.Docker/run-container.ps1
+++ src/docker/Thinktecture.Relay.Connector.Docker/run-container.ps1
@@ -2,6 +2,7 @@ docker rm -f relay_connector
 
 docker run `
   --name relay_connector `
+  --link relay_identityserver:relay_identityserver `
   --link relay_server:relay_server `
   -d `
   relay_connector
