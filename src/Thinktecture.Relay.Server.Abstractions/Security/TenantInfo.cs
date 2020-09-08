21b7475698c55c136211d3107d8ac9d5bd12c4ab
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2020-09-08 10:30:44 +0200

    refactor(server): move tenant information to separate class

---
+++ src/Thinktecture.Relay.Server.Abstractions/Security/TenantInfo.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Thinktecture.Relay.Server.Security
+{
+	/// <summary>
+	/// Provides information about a tenant.
+	/// </summary>
+	public class TenantInfo
+	{
+		/// <summary>
+		/// Gets or sets the id of the tenant.
+		/// </summary>
+		public Guid Id { get; set; }
+
+		/// <summary>
+		/// Gets or sets the name of the tenant.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Gets or sets the display name of the tenant.
+		/// </summary>
+		public string DisplayName { get; set; }
+	}
+}
