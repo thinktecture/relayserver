511ca7bdbde861409f5ef5686d36bd3eaae83a4d
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2020-09-09 13:05:19 +0200

    feat(connector): add some logging for signalr events

--- src/Thinktecture.Relay.Connector.Protocols.SignalR/ConnectorConnection.cs
+++ src/Thinktecture.Relay.Connector.Protocols.SignalR/ConnectorConnection.cs
@@ -18,6 +18,7 @@ namespace Thinktecture.Relay.Connector.Protocols.SignalR
 		private readonly HubConnection _connection;
 
 		private IConnectorTransport<TResponse> Transport => this;
+		private string _connectionId;
 
 		public ConnectorConnection(ILogger<ConnectorConnection<TRequest, TResponse>> logger, ConnectionFactory connectionFactory,
 			IClientRequestHandler<TRequest, TResponse> clientRequestHandler)
@@ -28,16 +29,47 @@ namespace Thinktecture.Relay.Connector.Protocols.SignalR
 			_connection = connectionFactory?.CreateConnection() ?? throw new ArgumentNullException(nameof(connectionFactory));
 			_connection.On<TRequest>("RequestTarget", RequestTargetAsync);
 
+			_connection.Closed += OnClosed;
+			_connection.Reconnecting += OnReconnecting;
+			_connection.Reconnected += OnReconnected;
+
 			_clientRequestHandler.Acknowledge += OnAcknowledge;
+		}
+
+		private Task OnClosed(Exception ex)
+		{
+			if (ex == null)
+			{
+				_logger.LogDebug("Connection {ConnectionId} gracefully closed", _connectionId);
+			}
+			else
+			{
+				_logger.LogError(ex, "Connection {ConnectionId} closed", _connectionId);
+			}
+			return Task.CompletedTask;
+		}
+
+		private Task OnReconnecting(Exception ex)
+		{
+			_logger.LogInformation(ex, "Trying to reconnect after connection was lost on connection {ConnectionId}", _connectionId);
+			_logger.LogTrace(ex, "Reconnecting on {ConnectionId}", _connectionId);
+			return Task.CompletedTask;
+		}
+
+		private Task OnReconnected(string connectionId)
+		{
+			_connectionId = connectionId;
+			_logger.LogInformation("Reconnected on connection {ConnectionId}", _connectionId);
+			return Task.CompletedTask;
 		}
 
 		private async Task OnAcknowledge(object sender, IAcknowledgeRequest request) => await Transport.AcknowledgeAsync(request);
 
 		private async Task RequestTargetAsync(TRequest request)
 		{
-			_logger.LogTrace("Received request {@Request}", request);
+			_logger.LogTrace("Received request {@Request} on connection {ConnectionId}", request, _connectionId);
 			var response = await _clientRequestHandler.HandleAsync(request, Transport.BinarySizeThreshold);
-			_logger.LogTrace("Received response {@Response}", response);
+			_logger.LogTrace("Received response {@Response} on connection {ConnectionId}", response, _connectionId);
 			await Transport.DeliverAsync(response);
 		}
 
@@ -45,19 +77,19 @@ namespace Thinktecture.Relay.Connector.Protocols.SignalR
 
 		Task IConnectorTransport<TResponse>.DeliverAsync(TResponse response)
 		{
-			_logger.LogTrace("Delivering response {@Response}", response);
+			_logger.LogTrace("Delivering response {@Response} on connection {ConnectionId}", response, _connectionId);
 			return _connection.InvokeAsync("Deliver", response);
 		}
 
 		Task IConnectorTransport<TResponse>.AcknowledgeAsync(IAcknowledgeRequest request)
 		{
-			_logger.LogTrace("Acknowledging request {@AcknowledgeRequest}", request);
+			_logger.LogTrace("Acknowledging request {@AcknowledgeRequest} on connection {ConnectionId}", request, _connectionId);
 			return _connection.InvokeAsync("Acknowledge", request);
 		}
 
 		Task IConnectorTransport<TResponse>.PongAsync()
 		{
-			_logger.LogTrace("Pong");
+			_logger.LogTrace("Pong on connection {ConnectionId}",  _connectionId);
 			return _connection.InvokeAsync("Pong");
 		}
 
@@ -65,14 +97,15 @@ namespace Thinktecture.Relay.Connector.Protocols.SignalR
 		{
 			_logger.LogDebug("Connecting");
 			await _connection.StartAsync(cancellationToken);
-			_logger.LogInformation("Connected");
+			_connectionId = _connection.ConnectionId;
+			_logger.LogInformation("Connected on connection {ConnectionId}", _connection.ConnectionId);
 		}
 
 		async Task IConnectorConnection.DisconnectAsync(CancellationToken cancellationToken)
 		{
 			_logger.LogDebug("Disconnecting");
 			await _connection.StopAsync(cancellationToken);
-			_logger.LogInformation("Disconnected");
+			_logger.LogInformation("Disconnected on connection {ConnectionId}", _connectionId);
 		}
 
 		public async ValueTask DisposeAsync()
