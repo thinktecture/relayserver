5e678dabb7ad3c48f3869df0562a16557a29ff09
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2020-09-11 12:28:05 +0200

    docs(management): add xml documentation

--- src/docker/Thinktecture.Relay.ManagementApi.Docker/Controllers/HealthController.cs
+++ src/docker/Thinktecture.Relay.ManagementApi.Docker/Controllers/HealthController.cs
@@ -3,16 +3,28 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Thinktecture.Relay.ManagementApi.Docker.Controllers
 {
+	/// <summary>
+	/// Provides health information for this API.
+	/// </summary>
 	[AllowAnonymous]
 	[Route("{controller}/{action}")]
+	[ApiExplorerSettings(IgnoreApi = true)]
 	public class HealthController : Controller
 	{
+		/// <summary>
+		/// Returns the ready state of this api.
+		/// </summary>
+		/// <returns>An <see cref="IActionResult"/> representing the state of the api.</returns>
 		[HttpGet]
 		public IActionResult Ready()
 		{
 			return Ok();
 		}
 
+		/// <summary>
+		/// Checks the health state of this api.
+		/// </summary>
+		/// <returns>An <see cref="IActionResult"/> representing the state of the api.</returns>
 		[HttpGet]
 		public IActionResult Check()
 		{
