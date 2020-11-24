7bc38a0e30cc202566a6b4d817ee9814acc59514
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2020-11-24 16:18:27 +0100

    build: ensure correct working directory

--- src/build-docker-images.ps1
+++ src/build-docker-images.ps1
@@ -1,5 +1,9 @@
+Push-Location $PSScriptRoot
+
 docker build . --force-rm -t relay_server -f ./docker/Thinktecture.Relay.Server.Docker/Dockerfile
 docker build . --force-rm -t relay_identityserver -f ./docker/Thinktecture.Relay.IdentityServer.Docker/Dockerfile
 docker build . --force-rm -t relay_management -f ./docker/Thinktecture.Relay.ManagementApi.Docker/Dockerfile
 docker build . --force-rm -t relay_statistics -f ./docker/Thinktecture.Relay.StatisticsApi.Docker/Dockerfile
 docker build . --force-rm -t relay_connector -f ./docker/Thinktecture.Relay.Connector.Docker/Dockerfile
+
+Pop-Location
