453d0c6de71918a55c7b218e22160df04fa7fe82
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2020-11-27 17:20:12 +0100

    feat(connector): expose connection relevant events

--- src/Thinktecture.Relay.Connector.Abstractions/IConnectorConnection.cs
+++ src/Thinktecture.Relay.Connector.Abstractions/IConnectorConnection.cs
@@ -9,6 +9,30 @@ namespace Thinktecture.Relay.Connector
 	public interface IConnectorConnection
 	{
 		/// <summary>
+		/// Event fired when a connection was established.
+		/// </summary>
+		/// <remarks>The event includes the connection id.</remarks>
+		event AsyncEventHandler<string> Connected;
+
+		/// <summary>
+		/// Event fired when a connection starts to reconnect.
+		/// </summary>
+		/// <remarks>The event includes the connection id.</remarks>
+		event AsyncEventHandler<string> Reconnecting;
+
+		/// <summary>
+		/// Event fired when a connection successfully reconnected.
+		/// </summary>
+		/// <remarks>The event includes the connection id.</remarks>
+		event AsyncEventHandler<string> Reconnected;
+
+		/// <summary>
+		/// Event fired when a connection was disconnected.
+		/// </summary>
+		/// <remarks>The event includes the connection id.</remarks>
+		event AsyncEventHandler<string> Disconnected;
+
+		/// <summary>
 		/// Opens a connection to the server.
 		/// </summary>
 		/// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is <see cref="CancellationToken.None"/>.</param>
