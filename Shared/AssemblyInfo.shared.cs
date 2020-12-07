e59bdcabc46f75ba504e51475e7e99967529254b
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2020-12-07 16:00:19 +0100

    version bumped

--- Shared/AssemblyInfo.shared.cs
+++ Shared/AssemblyInfo.shared.cs
@@ -7,6 +7,6 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Thinktecture AG 2015 - 2021. All rights reserved.")]
 [assembly: AssemblyTrademark("Thinktecture RelayServer")]
 
-[assembly: AssemblyVersion("2.4.0.0")]
-[assembly: AssemblyFileVersion("2.4.0.0")]
-[assembly: AssemblyInformationalVersion("2.4.0.0")]
+[assembly: AssemblyVersion("2.5.0.0")]
+[assembly: AssemblyFileVersion("2.5.0.0")]
+[assembly: AssemblyInformationalVersion("2.5.0.0-rc1")]
