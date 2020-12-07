42651557b886bdbefccf09593fa64eaa21f2e6b4
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2020-12-07 16:04:39 +0100

    fix(connector): more status codes without body

--- src/Thinktecture.Relay.Connector/Targets/HttpResponseTargetResponseFactory.cs
+++ src/Thinktecture.Relay.Connector/Targets/HttpResponseTargetResponseFactory.cs
@@ -1,8 +1,8 @@
 using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Thinktecture.Relay.Transport;
 
 namespace Thinktecture.Relay.Connector.Targets
@@ -15,9 +15,18 @@ namespace Thinktecture.Relay.Connector.Targets
 		public async Task<TResponse> CreateAsync(IClientRequest request, HttpResponseMessage message,
 			CancellationToken cancellationToken = default)
 		{
-			var hasBody = message.StatusCode != HttpStatusCode.NoContent;
+			var hasBody = (int)message.StatusCode switch
+			{
+				StatusCodes.Status100Continue => false,
+				StatusCodes.Status101SwitchingProtocols => false,
+				StatusCodes.Status102Processing => false,
+				StatusCodes.Status204NoContent => false,
+				StatusCodes.Status304NotModified => false,
+				_ => true
+			};
 
 			var response = request.CreateResponse<TResponse>();
+
 			response.HttpStatusCode = message.StatusCode;
 			response.HttpHeaders = message.Headers.Concat(message.Content.Headers).ToDictionary(h => h.Key, h => h.Value.ToArray());
 			response.BodySize = hasBody ? message.Content.Headers.ContentLength : 0;
