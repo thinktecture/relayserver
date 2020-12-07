f2ca4593d54ee72982600c165aa5f0f7b808c9fd
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2020-12-07 15:48:47 +0100

    feat(server): fill discovery document delay values from options

--- src/Thinktecture.Relay.Server/Services/DiscoveryDocumentBuilder.cs
+++ src/Thinktecture.Relay.Server/Services/DiscoveryDocumentBuilder.cs
@@ -13,14 +13,17 @@ namespace Thinktecture.Relay.Server.Services
 	public class DiscoveryDocumentBuilder
 	{
 		private readonly IServiceProvider _serviceProvider;
+		private readonly RelayServerOptions _relayServerOptions;
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="DiscoveryDocumentBuilder"/> class.
 		/// </summary>
 		/// <param name="serviceProvider">An <see cref="IServiceProvider"/>.</param>
-		public DiscoveryDocumentBuilder(IServiceProvider serviceProvider)
+		/// <param name="relayServerOptions">An <see cref="IOptions{TOptions}"/>.</param>
+		public DiscoveryDocumentBuilder(IServiceProvider serviceProvider, IOptions<RelayServerOptions> relayServerOptions)
 		{
 			_serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+			_relayServerOptions = relayServerOptions?.Value ?? throw new ArgumentNullException(nameof(relayServerOptions));
 		}
 
 		/// <summary>
@@ -41,18 +44,16 @@ namespace Thinktecture.Relay.Server.Services
 				RequestEndpoint = new Uri(baseUri, "body/request").ToString(),
 				ResponseEndpoint = new Uri(baseUri, "body/response").ToString(),
 				ConnectionTimeout = (int)TimeSpan.FromSeconds(30).TotalSeconds,
-				ReconnectMinDelay = (int)TimeSpan.FromSeconds(30).TotalSeconds,
-				ReconnectMaxDelay = (int)TimeSpan.FromMinutes(5).TotalSeconds,
+				ReconnectMinDelay = _relayServerOptions.ReconnectMinDelay,
+				ReconnectMaxDelay = _relayServerOptions.ReconnectMaxDelay
 			};
 		}
 
 		private Uri BuildBaseUri(HttpRequest request) => new Uri($"{request.Scheme}://{request.Host}{request.PathBase}", UriKind.Absolute);
 
 		private string GetServerVersion()
-		{
-			return GetType().Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+			=> GetType().Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
 				?? GetType().Assembly.GetCustomAttribute<AssemblyVersionAttribute>()?.Version;
-		}
 
 		private string GetAuthority()
 			=> _serviceProvider.GetService<IOptionsSnapshot<JwtBearerOptions>>()?.Get(Constants.DefaultAuthenticationScheme)?.Authority;
