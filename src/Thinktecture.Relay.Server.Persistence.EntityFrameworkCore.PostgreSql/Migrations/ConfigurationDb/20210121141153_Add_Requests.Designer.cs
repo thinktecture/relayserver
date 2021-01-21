0d59bee9e26f4700324dec583f3efa47cf768af8
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2021-01-21 13:20:01 +0100

    feat(persistence): add request model

--- src/Thinktecture.Relay.Server.Persistence.EntityFrameworkCore.PostgreSql/Migrations/ConfigurationDb/20210121141153_Add_Requests.Designer.cs
+++ src/Thinktecture.Relay.Server.Persistence.EntityFrameworkCore.PostgreSql/Migrations/ConfigurationDb/20210121141153_Add_Requests.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Thinktecture.Relay.Server.Persistence.EntityFrameworkCore;
@@ -9,9 +10,10 @@ using Thinktecture.Relay.Server.Persistence.EntityFrameworkCore;
 namespace Thinktecture.Relay.Server.Persistence.EntityFrameworkCore.PostgreSql.Migrations.ConfigurationDb
 {
     [DbContext(typeof(RelayDbContext))]
-    partial class RelayServerConfigurationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210121141153_Add_Requests")]
+    partial class Add_Requests
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Thinktecture.Relay.Server.Persistence.EntityFrameworkCore.SqlServer/Migrations/ConfigurationDb/20210121141157_Add_Requests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Thinktecture.Relay.Server.Persistence.EntityFrameworkCore.SqlServer.Migrations.ConfigurationDb
+{
+    public partial class Add_Requests : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Requests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TenantId = table.Column<Guid>(nullable: false),
+                    RequestId = table.Column<Guid>(nullable: false),
+                    RequestDate = table.Column<DateTimeOffset>(nullable: false),
+                    RequestDuration = table.Column<long>(nullable: false),
+                    RequestUrl = table.Column<string>(maxLength: 1000, nullable: false),
+                    Target = table.Column<string>(maxLength: 100, nullable: false),
+                    HttpMethod = table.Column<string>(maxLength: 10, nullable: false),
+                    RequestBodySize = table.Column<long>(nullable: false),
+                    HttpStatusCode = table.Column<int>(nullable: true),
+                    ResponseBodySize = table.Column<long>(nullable: true),
+                    Aborted = table.Column<bool>(nullable: false),
+                    Failed = table.Column<bool>(nullable: false),
+                    Expired = table.Column<bool>(nullable: false),
+                    Errored = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Requests_Tenants_TenantId",
+                        column: x => x.TenantId,
+                        principalTable: "Tenants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_TenantId",
+                table: "Requests",
+                column: "TenantId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Requests");
+        }
+    }
+}
