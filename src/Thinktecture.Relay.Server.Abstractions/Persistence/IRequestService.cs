9ef01f6dceb4d1d2b96eca2d9c380910a43dea6b
Boris Wilhelms <boris.wilhelms@thinktecture.com>
2022-10-06 15:46:10 +0200

    refactor: make request logging non cancelable

--- src/Thinktecture.Relay.Server.Abstractions/Persistence/IRequestService.cs
+++ src/Thinktecture.Relay.Server.Abstractions/Persistence/IRequestService.cs
@@ -13,10 +13,6 @@ public interface IRequestService
 	/// Stores the request.
 	/// </summary>
 	/// <param name="request">The <see cref="Request"/>.</param>
-	/// <param name="cancellationToken">
-	/// The token to monitor for cancellation requests. The default value is
-	/// <see cref="CancellationToken.None"/>.
-	/// </param>
 	/// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
-	Task StoreRequestAsync(Request request, CancellationToken cancellationToken);
+	Task StoreRequestAsync(Request request);
 }
--- src/Thinktecture.Relay.Server/Diagnostics/RelayRequestLogger.cs
+++ src/Thinktecture.Relay.Server/Diagnostics/RelayRequestLogger.cs
@@ -39,8 +39,7 @@ public partial class RelayRequestLogger<TRequest, TResponse> : IRelayRequestLogg
 	partial void LogSuccess(Guid requestId);
 
 	/// <inheritdoc/>
-	public async Task LogSuccessAsync(IRelayContext<TRequest, TResponse> relayContext,
-		CancellationToken cancellationToken = default)
+	public async Task LogSuccessAsync(IRelayContext<TRequest, TResponse> relayContext)
 	{
 		if (!_relayServerOptions.RequestLoggerLevel.LogSucceeded()) return;
 
@@ -49,15 +48,14 @@ public partial class RelayRequestLogger<TRequest, TResponse> : IRelayRequestLogg
 		var request = CreateRequest(relayContext);
 		request.HttpStatusCode = relayContext.TargetResponse?.HttpStatusCode;
 		request.ResponseBodySize = relayContext.TargetResponse?.BodySize;
-		await _requestService.StoreRequestAsync(request, cancellationToken);
+		await _requestService.StoreRequestAsync(request);
 	}
 
 	[LoggerMessage(20401, LogLevel.Trace, "Writing request log for aborted request {RequestId}")]
 	partial void LogAbort(Guid requestId);
 
 	/// <inheritdoc/>
-	public async Task LogAbortAsync(IRelayContext<TRequest, TResponse> relayContext,
-		CancellationToken cancellationToken = default)
+	public async Task LogAbortAsync(IRelayContext<TRequest, TResponse> relayContext)
 	{
 		if (!_relayServerOptions.RequestLoggerLevel.LogAborted()) return;
 
@@ -65,15 +63,14 @@ public partial class RelayRequestLogger<TRequest, TResponse> : IRelayRequestLogg
 
 		var request = CreateRequest(relayContext);
 		request.Aborted = true;
-		await _requestService.StoreRequestAsync(request, cancellationToken);
+		await _requestService.StoreRequestAsync(request);
 	}
 
 	[LoggerMessage(20402, LogLevel.Trace, "Writing request log for failed request {RequestId}")]
 	partial void LogFail(Guid requestId);
 
 	/// <inheritdoc/>
-	public async Task LogFailAsync(IRelayContext<TRequest, TResponse> relayContext,
-		CancellationToken cancellationToken = default)
+	public async Task LogFailAsync(IRelayContext<TRequest, TResponse> relayContext)
 	{
 		if (!_relayServerOptions.RequestLoggerLevel.LogFailed()) return;
 
@@ -81,15 +78,14 @@ public partial class RelayRequestLogger<TRequest, TResponse> : IRelayRequestLogg
 
 		var request = CreateRequest(relayContext);
 		request.Failed = true;
-		await _requestService.StoreRequestAsync(request, cancellationToken);
+		await _requestService.StoreRequestAsync(request);
 	}
 
 	[LoggerMessage(20403, LogLevel.Trace, "Writing request log for expired request {RequestId}")]
 	partial void LogExpired(Guid requestId);
 
 	/// <inheritdoc/>
-	public async Task LogExpiredAsync(IRelayContext<TRequest, TResponse> relayContext,
-		CancellationToken cancellationToken = default)
+	public async Task LogExpiredAsync(IRelayContext<TRequest, TResponse> relayContext)
 	{
 		if (!_relayServerOptions.RequestLoggerLevel.LogExpired()) return;
 
@@ -97,15 +93,14 @@ public partial class RelayRequestLogger<TRequest, TResponse> : IRelayRequestLogg
 
 		var request = CreateRequest(relayContext);
 		request.Expired = true;
-		await _requestService.StoreRequestAsync(request, cancellationToken);
+		await _requestService.StoreRequestAsync(request);
 	}
 
 	[LoggerMessage(20404, LogLevel.Trace, "Writing request log for error on request {RequestId}")]
 	partial void LogError(Guid requestId);
 
 	/// <inheritdoc/>
-	public async Task LogErrorAsync(IRelayContext<TRequest, TResponse> relayContext,
-		CancellationToken cancellationToken = default)
+	public async Task LogErrorAsync(IRelayContext<TRequest, TResponse> relayContext)
 	{
 		if (!_relayServerOptions.RequestLoggerLevel.LogErrored()) return;
 
@@ -113,7 +108,7 @@ public partial class RelayRequestLogger<TRequest, TResponse> : IRelayRequestLogg
 
 		var request = CreateRequest(relayContext);
 		request.Errored = true;
-		await _requestService.StoreRequestAsync(request, cancellationToken);
+		await _requestService.StoreRequestAsync(request);
 	}
 
 	private Request CreateRequest(IRelayContext<TRequest, TResponse> relayContext)
