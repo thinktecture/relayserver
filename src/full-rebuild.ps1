2a6addbdf850d208304af5b6c2295a4797ee6f93
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2023-11-07 15:13:17 +0100

    chore: update build-release script

--- src/full-rebuild.ps1
+++ src/full-rebuild.ps1
@@ -1,4 +1,8 @@
 Get-ChildItem .\ -include bin,obj -Recurse | foreach ($_) { remove-item $_.fullname -Force -Recurse }
 # dotnet nuget locals --clear all
 
-dotnet build
+dotnet restore
+dotnet build --configuration Release
+dotnet pack --configuration Release --output ./packages --no-build
+# cd packages
+# dotnet nuget push *.nupkg --source nuget.org --api-key {API_KEY_HERE}
