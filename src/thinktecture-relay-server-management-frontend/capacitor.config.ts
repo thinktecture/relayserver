f62e64526d63f71ed94c403c280a1c59f30b313d
Felix Schütz <felix.schuetz@thinktecture.com>
2024-01-29 16:53:52 +0100

    Initialize Ionic project

---
+++ src/thinktecture-relay-server-management-frontend/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'thinktecture-relay-server-management-frontend',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
