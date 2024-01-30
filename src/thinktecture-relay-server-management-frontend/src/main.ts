7468298e0cef66a1a8ea83a7340746087ed56ef2
Felix Schütz <felix.schuetz@thinktecture.com>
2024-01-30 08:16:06 +0100

    Fetch tenants from backend

--- src/thinktecture-relay-server-management-frontend/src/main.ts
+++ src/thinktecture-relay-server-management-frontend/src/main.ts
@@ -17,5 +17,6 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes),
+    provideHttpClient(),
   ],
 })
