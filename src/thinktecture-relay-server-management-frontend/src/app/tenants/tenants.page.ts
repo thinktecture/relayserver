a654b38bb6928372f975577815b89c38b98f11d4
Felix Schütz <felix.schuetz@thinktecture.com>
2024-01-30 12:50:17 +0100

    Use ApiService to list tenants

--- src/thinktecture-relay-server-management-frontend/src/app/tenants/tenants.page.ts
+++ src/thinktecture-relay-server-management-frontend/src/app/tenants/tenants.page.ts
@@ -1,24 +1,35 @@
-import { Component, inject } from '@angular/core'
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone'
-import { HttpClient } from '@angular/common/http'
-import { JsonPipe } from '@angular/common'
-import { toSignal } from '@angular/core/rxjs-interop'
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import {
+  IonContent,
+  IonHeader,
+  IonInfiniteScroll,
+  IonLabel,
+  IonList,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { ApiService } from '../api/api.service';
 
 @Component({
   selector: 'app-tenants',
   templateUrl: 'tenants.page.html',
   styleUrls: ['tenants.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, JsonPipe],
+  imports: [
+    JsonPipe,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonList,
+    IonInfiniteScroll,
+    IonLabel,
+  ],
 })
 export class TenantsPage {
-  private httpClient = inject(HttpClient)
+  private api = inject(ApiService);
 
-  tenants = toSignal(
-    this.httpClient.get('/api/management/tenants', {
-      headers: {
-        'TT-Api-Key': 'readwrite-key',
-      },
-    }),
-  )
+  tenants = toSignal(this.api.getTenantsPaged());
 }
