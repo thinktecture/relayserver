4126b9b5abb1b5979fce5974753ccbe1aab02ff3
Felix Schütz <felix.schuetz@thinktecture.com>
2024-01-30 18:16:21 +0100

    Show tenant details

--- src/thinktecture-relay-server-management-frontend/src/app/tenant-details/tenant-details.page.ts
+++ src/thinktecture-relay-server-management-frontend/src/app/tenant-details/tenant-details.page.ts
@@ -1,13 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { Component, inject, input } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { IonicModule } from '@ionic/angular';
+import { switchMap } from 'rxjs';
+import { ApiService } from '../api/api.service';
 
 @Component({
   selector: 'app-tenant-details',
   templateUrl: './tenant-details.page.html',
   styleUrls: ['./tenant-details.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [AsyncPipe, JsonPipe, IonicModule],
 })
 export class TenantDetailsPage {
-  @Input() name = '';
+  private api = inject(ApiService);
+
+  name = input.required<string>();
+
+  details = toObservable(this.name).pipe(
+    switchMap((name) => this.api.getSingleTenant(name)),
+  );
 }
