cee9a27ace9b4e2c45de1f7bfa4ab17f13c892b7
Felix Schütz <felix.schuetz@thinktecture.com>
2024-01-30 08:08:53 +0100

    Format files

--- src/thinktecture-relay-server-management-frontend/src/app/tab2/tab2.page.ts
+++ src/thinktecture-relay-server-management-frontend/src/app/tab2/tab2.page.ts
@@ -1,16 +1,12 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Component } from '@angular/core'
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone'
+import { ExploreContainerComponent } from '../explore-container/explore-container.component'
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
-export class Tab2Page {
-
-  constructor() {}
-
-}
+export class Tab2Page {}
