147a4fdbdb22e0b46bb8e5d8699ccbf6a44a4772
Felix Schütz <felix.schuetz@thinktecture.com>
2024-01-30 08:16:06 +0100

    Fetch tenants from backend

--- src/thinktecture-relay-server-management-frontend/src/app/tenants/tenants.page.ts
+++ src/thinktecture-relay-server-management-frontend/src/app/tenants/tenants.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core'
+import { Component, inject } from '@angular/core'
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone'
-import { ExploreContainerComponent } from '../explore-container/explore-container.component'
+import { HttpClient } from '@angular/common/http'
+import { JsonPipe } from '@angular/common'
+import { toSignal } from '@angular/core/rxjs-interop'
 
 @Component({
   selector: 'app-tenants',
   templateUrl: 'tenants.page.html',
   styleUrls: ['tenants.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, JsonPipe],
 })
-export class TenantsPage {}
+export class TenantsPage {
+  private httpClient = inject(HttpClient)
+
+  tenants = toSignal(
+    this.httpClient.get('/api/management/tenants', {
+      headers: {
+        'TT-Api-Key': 'readwrite-key',
+      },
+    }),
+  )
+}
