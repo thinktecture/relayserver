0eb2736ad591aa3c35099c3ff69422e767689e6b
Felix Schütz <felix.schuetz@thinktecture.com>
2024-01-31 11:54:04 +0100

    Add done button to new tenant modal

--- src/thinktecture-relay-server-management-frontend/src/app/new-tenant/new-tenant.component.ts
+++ src/thinktecture-relay-server-management-frontend/src/app/new-tenant/new-tenant.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  EventEmitter,
-  OnInit,
-  Output,
-  ViewChild,
-} from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
 import {
   IonButton,
   IonButtons,
