1870c2be8e2ff286e7499781d201582375198d0f
Felix Schütz <felix.schuetz@thinktecture.com>
2024-01-31 13:43:14 +0100

    Add more inputs to new tenant component

--- src/thinktecture-relay-server-management-frontend/src/app/new-tenant/new-tenant.component.ts
+++ src/thinktecture-relay-server-management-frontend/src/app/new-tenant/new-tenant.component.ts
@@ -2,14 +2,20 @@ import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
 import {
   IonButton,
   IonButtons,
+  IonCheckbox,
   IonContent,
   IonHeader,
+  IonIcon,
   IonInput,
   IonItem,
+  IonLabel,
   IonList,
+  IonTextarea,
   IonTitle,
   IonToolbar,
 } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { addCircle } from 'ionicons/icons';
 
 @Component({
   selector: 'app-new-tenant',
@@ -26,12 +32,20 @@ import {
     IonList,
     IonItem,
     IonInput,
+    IonTextarea,
+    IonCheckbox,
+    IonIcon,
+    IonLabel,
   ],
 })
 export class NewTenantComponent {
   @Output() dismiss = new EventEmitter<void>();
 
   @ViewChild('tenantName') tenantName: IonInput | null = null;
+
+  constructor() {
+    addIcons({ addCircle });
+  }
 
   focus() {
     this.tenantName?.setFocus();
