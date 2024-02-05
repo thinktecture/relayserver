42f509c21eb13e9f86c05abb3c53f0a04a77ca32
Felix Schütz <felix.schuetz@thinktecture.com>
2024-02-05 15:17:32 +0100

    Extract tenant details into new component

---
+++ src/thinktecture-relay-server-management-frontend/src/app/view-tenant/view-tenant.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { Tenant } from '../api/tenant.model';
+import { IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-view-tenant',
+  templateUrl: './view-tenant.component.html',
+  styleUrls: ['./view-tenant.component.scss'],
+  standalone: true,
+  imports: [IonList, IonItem, IonLabel],
+})
+export class ViewTenantComponent {
+  tenant = input.required<Tenant>();
+}
