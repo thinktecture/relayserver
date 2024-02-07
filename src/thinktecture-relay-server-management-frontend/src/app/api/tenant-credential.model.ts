b3cefaea7c23a806e9dbe6d94d9e6e2cdc1bfdce
Felix Schütz <felix.schuetz@thinktecture.com>
2024-02-07 15:32:16 +0100

    Store credential ID and created date in form group

--- src/thinktecture-relay-server-management-frontend/src/app/api/tenant-credential.model.ts
+++ src/thinktecture-relay-server-management-frontend/src/app/api/tenant-credential.model.ts
@@ -3,9 +3,6 @@ import { NewTenantCredential } from './new-tenant-credential.model';
 /** Represents a stored credential for a tenant. */
 export interface TenantCredential
   extends Omit<NewTenantCredential, 'plainTextValue'> {
-  /** Unique identifier for this credential. */
-  id: string;
-
   /** Date and time this credential was created at. */
   created: string;
 }
