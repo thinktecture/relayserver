5d2fbec6d55bd0a486119ab140a9c94ee1368287
Felix Schütz <felix.schuetz@thinktecture.com>
2024-02-09 19:36:45 +0100

    Add API auth service to store auth details

--- src/frontend/src/app/api/api.interceptor.ts
+++ src/frontend/src/app/api/api.interceptor.ts
@@ -1,8 +1,15 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { ApiAuthService } from './api-auth.service';
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
+  const apiAuth = inject(ApiAuthService);
+  if (apiAuth.headerName === undefined || apiAuth.key === undefined) {
+    return next(req);
+  }
+
   const reqWithAuth = req.clone({
-    setHeaders: { 'TT-Api-Key': 'readwrite-key' },
+    setHeaders: { [apiAuth.headerName]: apiAuth.key },
   });
 
   return next(reqWithAuth);
