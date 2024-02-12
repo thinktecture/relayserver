5e12c6b86bc689eedd19fbab9c36ea69785f1fbe
Felix Schütz <felix.schuetz@thinktecture.com>
2024-02-12 10:00:31 +0100

    Show HTTP errors in toast

--- src/frontend/src/app/api/api.interceptor.ts
+++ src/frontend/src/app/api/api.interceptor.ts
@@ -1,5 +1,8 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular/standalone';
+import { catchError } from 'rxjs';
 import { ApiAuthService } from './api-auth.service';
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
@@ -12,5 +15,34 @@ export const apiInterceptor: HttpInterceptorFn = (req, next) => {
     setHeaders: { [apiAuth.headerName]: apiAuth.key },
   });
 
-  return next(reqWithAuth);
+  const toastService = inject(ToastController);
+  const router = inject(Router);
+  return next(reqWithAuth).pipe(
+    catchError(async (err: HttpErrorResponse) => {
+      let toast;
+      if (err.status === 401) {
+        toast = await toastService.create({
+          message: `Unauthorized (${err.status})`,
+          color: 'danger',
+          duration: 5000,
+          buttons: [
+            {
+              text: 'Set API key',
+              handler: () => router.navigate(['/sign-in']),
+            },
+          ],
+        });
+      } else {
+        toast = await toastService.create({
+          message: `An unknown error occurred (${err.status})`,
+          color: 'danger',
+          duration: 5000,
+        });
+      }
+
+      await toast.present();
+
+      throw err;
+    }),
+  );
 };
