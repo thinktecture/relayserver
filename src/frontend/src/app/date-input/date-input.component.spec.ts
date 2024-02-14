41a3399d96aac979aa7bb5e079492a2c4a8a17f2
Felix Schütz <felix.schuetz@thinktecture.com>
2024-02-14 17:00:10 +0100

    Add date input component

---
+++ src/frontend/src/app/date-input/date-input.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DateInputComponent } from './date-input.component';
+
+describe('DateInputComponent', () => {
+  let component: DateInputComponent;
+  let fixture: ComponentFixture<DateInputComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DateInputComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(DateInputComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
