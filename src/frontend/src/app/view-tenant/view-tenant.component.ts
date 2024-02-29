5694565bb274b633257e046df10206324f54784e
Felix Schütz <felix.schuetz@thinktecture.com>
2024-02-29 16:31:36 +0100

    Use DatePipe to format dates

--- src/frontend/src/app/view-tenant/view-tenant.component.ts
+++ src/frontend/src/app/view-tenant/view-tenant.component.ts
@@ -1,13 +1,14 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { Tenant } from '../api/tenant.model';
 import { IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-view-tenant',
   templateUrl: './view-tenant.component.html',
   styleUrls: ['./view-tenant.component.scss'],
   standalone: true,
-  imports: [IonList, IonItem, IonLabel],
+  imports: [DatePipe, IonList, IonItem, IonLabel],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ViewTenantComponent {
