7ae2af9413d51d7462c2423651a128890b67b7f5
Felix Schütz <felix.schuetz@thinktecture.com>
2024-03-01 14:28:49 +0100

    Test ng2-charts

--- src/frontend/src/app/stats/stats.page.ts
+++ src/frontend/src/app/stats/stats.page.ts
@@ -5,13 +5,37 @@ import {
   IonTitle,
   IonContent,
 } from '@ionic/angular/standalone';
+import { ChartConfiguration } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-stats',
   templateUrl: 'stats.page.html',
   styleUrls: ['stats.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, BaseChartDirective],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StatsPage {}
+export class StatsPage {
+  data: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [65, 59, 80, 81, 56, 55, 40],
+        label: 'Series A',
+      },
+      {
+        data: [28, 48, 40, 19, 86, 27, 90],
+        label: 'Series B',
+      },
+    ],
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  };
+
+  options: ChartConfiguration['options'] = {
+    elements: {
+      line: {
+        tension: 0.5,
+      },
+    },
+  };
+}
