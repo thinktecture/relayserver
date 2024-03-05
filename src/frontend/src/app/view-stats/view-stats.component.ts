526898308a5c6953541566360da1b82c5e66918c
Felix Schütz <felix.schuetz@thinktecture.com>
2024-03-05 09:30:33 +0100

    Move chart rendering into new component

---
+++ src/frontend/src/app/view-stats/view-stats.component.ts
@@ -0,0 +1,32 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChartConfiguration } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+
+@Component({
+  selector: 'app-view-stats',
+  templateUrl: './view-stats.component.html',
+  styleUrl: './view-stats.component.css',
+  standalone: true,
+  imports: [BaseChartDirective],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ViewStatsComponent {
+  data = input.required<ChartConfiguration['data']>();
+
+  options: ChartConfiguration['options'] = {
+    elements: {
+      line: {
+        tension: 0.5,
+      },
+    },
+    scales: {
+      y: {
+        title: { display: true, text: 'Count' },
+      },
+      yRight: {
+        position: 'right',
+        title: { display: true, text: 'Size in bytes' },
+      },
+    },
+  };
+}
