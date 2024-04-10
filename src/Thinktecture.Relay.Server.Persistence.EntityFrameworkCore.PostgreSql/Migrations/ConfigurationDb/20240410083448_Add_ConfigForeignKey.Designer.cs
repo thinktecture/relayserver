71c7dbf0245b77ed1c61124b3c0d534eaab51314
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2024-04-10 11:02:33 +0200

    fix(persistence): add missing config foreign key configuration to tenant

--- src/Thinktecture.Relay.Server.Persistence.EntityFrameworkCore.PostgreSql/Migrations/ConfigurationDb/20240410083448_Add_ConfigForeignKey.Designer.cs
+++ src/Thinktecture.Relay.Server.Persistence.EntityFrameworkCore.PostgreSql/Migrations/ConfigurationDb/20240410083448_Add_ConfigForeignKey.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Thinktecture.Relay.Server.Persistence.EntityFrameworkCore;
@@ -11,9 +12,10 @@ using Thinktecture.Relay.Server.Persistence.EntityFrameworkCore;
 namespace Thinktecture.Relay.Server.Persistence.EntityFrameworkCore.PostgreSql.Migrations.ConfigurationDb
 {
     [DbContext(typeof(RelayDbContext))]
-    partial class RelayDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240410083448_Add_ConfigForeignKey")]
+    partial class Add_ConfigForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Thinktecture.Relay.Server.Persistence.EntityFrameworkCore.PostgreSql/Migrations/ConfigurationDb/20240410083448_Add_ConfigForeignKey.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Thinktecture.Relay.Server.Persistence.EntityFrameworkCore.PostgreSql.Migrations.ConfigurationDb
+{
+    public partial class Add_ConfigForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tenants_Configs_ConfigTenantName",
+                table: "Tenants");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tenants_ConfigTenantName",
+                table: "Tenants");
+
+            migrationBuilder.DropColumn(
+                name: "ConfigTenantName",
+                table: "Tenants");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TenantName",
+                table: "Configs",
+                type: "character varying(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Configs_Tenants_TenantName",
+                table: "Configs",
+                column: "TenantName",
+                principalTable: "Tenants",
+                principalColumn: "NormalizedName",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Configs_Tenants_TenantName",
+                table: "Configs");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConfigTenantName",
+                table: "Tenants",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TenantName",
+                table: "Configs",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tenants_ConfigTenantName",
+                table: "Tenants",
+                column: "ConfigTenantName");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tenants_Configs_ConfigTenantName",
+                table: "Tenants",
+                column: "ConfigTenantName",
+                principalTable: "Configs",
+                principalColumn: "TenantName");
+        }
+    }
+}
