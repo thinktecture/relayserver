f4740fa156a7c4fa6da52b26a97ed37e2b667961
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2024-05-07 14:02:21 +0200

    feat(idsrv): consolidate logging

---
+++ src/Thinktecture.Relay.IdentityServer/LoggingEventIds.cs
@@ -0,0 +1,10 @@
+namespace Thinktecture.Relay.IdentityServer;
+
+internal static class LoggingEventIds
+{
+	public const int RotateCertificateFileStoreRebuildingCertificateCache = 1;
+	public const int RotateCertificateFileStoreCreateCertificate = 2;
+	public const int RotateCertificateFileStoreRemoveOutdatedCertificate = 3;
+	public const int RotateCertificateFileStoreLoadCertificate = 4;
+	public const int RotateCertificateFileStoreDeleteCertificateFile = 5;
+}
