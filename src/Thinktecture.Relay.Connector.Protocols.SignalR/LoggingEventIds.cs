650ae00b10001df817d36c86d9125b68e54ce02c
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2024-05-15 12:24:35 +0200

    refactor(protocols): group logging ids

--- src/Thinktecture.Relay.Connector.Protocols.SignalR/LoggingEventIds.cs
+++ src/Thinktecture.Relay.Connector.Protocols.SignalR/LoggingEventIds.cs
@@ -2,29 +2,29 @@ namespace Thinktecture.Relay.Connector.Protocols.SignalR;
 
 internal static class LoggingEventIds
 {
-	public const int AcknowledgeTransportTransportingAck = 1;
-	public const int AcknowledgeTransportErrorTransportingAck = 2;
+	public const int AcknowledgeTransportTransportingAck = 10001;
+	public const int AcknowledgeTransportErrorTransportingAck = 10002;
 
-	public const int ConnectorConnectionDisconnecting = 3;
-	public const int ConnectorConnectionDisconnected = 4;
-	public const int ConnectorConnectionGracefullyClosed = 5;
-	public const int ConnectorConnectionReconnectingAfterLoss = 6;
-	public const int ConnectorConnectionReconnectingAfterError = 7;
-	public const int ConnectorConnectionReconnectedWithoutId = 9;
-	public const int ConnectorConnectionReconnected = 10;
-	public const int ConnectorConnectionReconnectedWithNewId = 11;
-	public const int ConnectorConnectionHandlingRequestDetailed = 12;
-	public const int ConnectorConnectionHandlingRequestSimple = 13;
-	public const int ConnectorConnectionReceivedTenantConfig = 14;
-	public const int ConnectorConnectionLogConnected = 15;
-	public const int ConnectorConnectionConnectError = 16;
+	public const int ConnectorConnectionDisconnecting = 10101;
+	public const int ConnectorConnectionDisconnected = 10102;
+	public const int ConnectorConnectionGracefullyClosed = 10103;
+	public const int ConnectorConnectionReconnectingAfterLoss = 10104;
+	public const int ConnectorConnectionReconnectingAfterError = 10105;
+	public const int ConnectorConnectionReconnectedWithoutId = 10106;
+	public const int ConnectorConnectionReconnected = 10107;
+	public const int ConnectorConnectionReconnectedWithNewId = 10108;
+	public const int ConnectorConnectionHandlingRequestDetailed = 10109;
+	public const int ConnectorConnectionHandlingRequestSimple = 10110;
+	public const int ConnectorConnectionReceivedTenantConfig = 10111;
+	public const int ConnectorConnectionLogConnected = 10112;
+	public const int ConnectorConnectionConnectError = 10113;
 
-	public const int DiscoveryDocumentRetryPolicyLogRetry = 17;
-	public const int DiscoveryDocumentKeepingDefaults = 18;
-	public const int DiscoveryDocumentUsingDelays = 19;
+	public const int DiscoveryDocumentRetryPolicyLogRetry = 10201;
+	public const int DiscoveryDocumentKeepingDefaults = 10202;
+	public const int DiscoveryDocumentUsingDelays = 10203;
 
-	public const int HubConnectionFactoryCreatingConnection = 20;
+	public const int HubConnectionFactoryCreatingConnection = 10301;
 
-	public const int ResponseTransportTransportingResponse = 21;
-	public const int ResponseTransportErrorTransportingResponse = 22;
+	public const int ResponseTransportTransportingResponse = 10401;
+	public const int ResponseTransportErrorTransportingResponse = 10402;
 }
--- src/Thinktecture.Relay.Server.Protocols.RabbitMq/LoggingEventIds.cs
+++ src/Thinktecture.Relay.Server.Protocols.RabbitMq/LoggingEventIds.cs
@@ -2,26 +2,26 @@ namespace Thinktecture.Relay.Server.Protocols.RabbitMq;
 
 internal static class LoggingEventIds
 {
-	public const int ModelExtensionsConsumingConsumer = 1;
-	public const int ModelExtensionsLostConsumer = 2;
-	public const int ModelExtensionsRestoredConsumer = 3;
+	public const int ModelExtensionsConsumingConsumer = 10001;
+	public const int ModelExtensionsLostConsumer = 10002;
+	public const int ModelExtensionsRestoredConsumer = 10003;
 
-	public const int ModelFactoryConnectionRecovered = 4;
-	public const int ModelFactoryConnectionClosed = 5;
-	public const int ModelFactoryModelCreated = 6;
-	public const int ModelFactoryModelCallbackError = 7;
-	public const int ModelFactoryModelClosed = 8;
+	public const int ModelFactoryConnectionRecovered = 10101;
+	public const int ModelFactoryConnectionClosed = 10102;
+	public const int ModelFactoryModelCreated = 10103;
+	public const int ModelFactoryModelCallbackError = 10104;
+	public const int ModelFactoryModelClosed = 10105;
 
-	public const int ServerTransportDispatchingAcknowledge = 9;
-	public const int ServerTransportDispatchedResponse = 10;
-	public const int ServerTransportDispatchedAcknowledge = 11;
-	public const int ServerTransportResponseConsumed = 12;
-	public const int ServerTransportAcknowledgeConsumed = 13;
+	public const int ServerTransportDispatchingAcknowledge = 10201;
+	public const int ServerTransportDispatchedResponse = 10202;
+	public const int ServerTransportDispatchedAcknowledge = 10203;
+	public const int ServerTransportResponseConsumed = 10204;
+	public const int ServerTransportAcknowledgeConsumed = 10205;
 
-	public const int TenantHandlerAcknowledge = 14;
-	public const int TenantHandlerCouldNotParseAcknowledge = 15;
-	public const int TenantHandlerReceivedRequest = 16;
+	public const int TenantHandlerAcknowledge = 10301;
+	public const int TenantHandlerCouldNotParseAcknowledge = 10302;
+	public const int TenantHandlerReceivedRequest = 10303;
 
-	public const int TenantTransportPublishedRequest = 17;
-	public const int TenantTransportErrorDispatchingRequest = 18;
+	public const int TenantTransportPublishedRequest = 10401;
+	public const int TenantTransportErrorDispatchingRequest = 10402;
 }
