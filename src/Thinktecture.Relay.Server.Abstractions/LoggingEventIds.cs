4d9567c949c8cd92415740831a1714a98daaa5d8
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2024-05-15 12:25:19 +0200

    refactor(server-abstractions): group logging ids

--- src/Thinktecture.Relay.Server.Abstractions/LoggingEventIds.cs
+++ src/Thinktecture.Relay.Server.Abstractions/LoggingEventIds.cs
@@ -2,10 +2,10 @@ namespace Thinktecture.Relay.Server;
 
 internal static class LoggingEventIds
 {
-	public const int ConnectorRegistryRegisteringConnection = 1;
-	public const int ConnectorRegistryUnregisteringConnection = 2;
-	public const int ConnectorRegistryCouldNotUnregisterConnection = 3;
-	public const int ConnectorRegistryUnknownRequestConnection = 4;
-	public const int ConnectorRegistryUnknownAcknowledgeConnection = 5;
-	public const int ConnectorRegistryDeliveringRequest = 6;
+	public const int ConnectorRegistryRegisteringConnection = 10001;
+	public const int ConnectorRegistryUnregisteringConnection = 10002;
+	public const int ConnectorRegistryCouldNotUnregisterConnection = 10003;
+	public const int ConnectorRegistryUnknownRequestConnection = 10004;
+	public const int ConnectorRegistryUnknownAcknowledgeConnection = 10005;
+	public const int ConnectorRegistryDeliveringRequest = 10006;
 }
