a9f854040fb4a40864b662bb1e7bcb8787a524a8
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2024-05-15 12:25:06 +0200

    refactor(connector): group logging ids

--- src/Thinktecture.Relay.Connector/LoggingEventIds.cs
+++ src/Thinktecture.Relay.Connector/LoggingEventIds.cs
@@ -2,39 +2,39 @@ namespace Thinktecture.Relay.Connector;
 
 internal static class LoggingEventIds
 {
-	public const int AccessTokenProviderRequestingAccessToken = 1;
-	public const int AccessTokenManagementConfigureOptionsGotDiscoveryDocument = 2;
-	public const int AccessTokenManagementConfigureOptionsErrorRetrievingDiscoveryDocument = 3;
-	public const int AccessTokenManagementConfigureOptionsErrorTargetTypeNotFound = 4;
-	public const int AccessTokenManagementConfigureOptionsCouldNotParseTimeout = 5;
+	public const int AccessTokenProviderRequestingAccessToken = 10001;
+	public const int AccessTokenManagementConfigureOptionsGotDiscoveryDocument = 10002;
+	public const int AccessTokenManagementConfigureOptionsErrorRetrievingDiscoveryDocument = 10003;
+	public const int AccessTokenManagementConfigureOptionsErrorTargetTypeNotFound = 10004;
+	public const int AccessTokenManagementConfigureOptionsCouldNotParseTimeout = 10005;
 
-	public const int RelayConnectorPostConfigureOptionsGotDiscoveryDocument = 6;
+	public const int RelayConnectorPostConfigureOptionsGotDiscoveryDocument = 10101;
 
-	public const int ClientRequestHandlerAcknowledgeRequest = 7;
-	public const int ClientRequestHandlerErrorHandlingRequest = 8;
-	public const int ClientRequestHandlerDeliverResponse = 9;
-	public const int ClientRequestHandlerDiscardResponse = 10;
+	public const int ClientRequestHandlerAcknowledgeRequest = 10201;
+	public const int ClientRequestHandlerErrorHandlingRequest = 10202;
+	public const int ClientRequestHandlerDeliverResponse = 10203;
+	public const int ClientRequestHandlerDiscardResponse = 10204;
 
-	public const int ClientRequestWorkerNoTargetFound = 11;
-	public const int ClientRequestWorkerFoundTarget = 12;
-	public const int ClientRequestWorkerRequestingBody = 13;
-	public const int ClientRequestWorkerRequestingTarget = 14;
-	public const int ClientRequestWorkerOutsourcingUnknownBody = 15;
-	public const int ClientRequestWorkerOutsourcingBody = 16;
-	public const int ClientRequestWorkerOutsourcedBody = 17;
-	public const int ClientRequestWorkerOutsourcingBodyFailed = 18;
-	public const int ClientRequestWorkerErrorOutsourcingBody = 19;
-	public const int ClientRequestWorkerInlineBody = 20;
-	public const int ClientRequestWorkerErrorDownloadingBody = 21;
-	public const int ClientRequestWorkerRequestTimedOut = 22;
-	public const int ClientRequestWorkerErrorProcessingRequest = 23;
-	public const int ClientRequestWorkerUploadingBodyFailed = 24;
-	public const int ClientRequestWorkerErrorUploadingBody = 25;
+	public const int ClientRequestWorkerNoTargetFound = 10301;
+	public const int ClientRequestWorkerFoundTarget = 10302;
+	public const int ClientRequestWorkerRequestingBody = 10303;
+	public const int ClientRequestWorkerRequestingTarget = 10304;
+	public const int ClientRequestWorkerOutsourcingUnknownBody = 10305;
+	public const int ClientRequestWorkerOutsourcingBody = 10306;
+	public const int ClientRequestWorkerOutsourcedBody = 10307;
+	public const int ClientRequestWorkerOutsourcingBodyFailed = 10308;
+	public const int ClientRequestWorkerErrorOutsourcingBody = 10309;
+	public const int ClientRequestWorkerInlineBody = 10310;
+	public const int ClientRequestWorkerErrorDownloadingBody = 10311;
+	public const int ClientRequestWorkerRequestTimedOut = 10312;
+	public const int ClientRequestWorkerErrorProcessingRequest = 10313;
+	public const int ClientRequestWorkerUploadingBodyFailed = 10314;
+	public const int ClientRequestWorkerErrorUploadingBody = 10315;
 
-	public const int RelayTargetRegistryRegisteredTarget = 26;
-	public const int RelayTargetRegistryUnregisteredTarget = 27;
-	public const int RelayTargetRegistryCouldNotUnregisterTarget = 28;
+	public const int RelayTargetRegistryRegisteredTarget = 10401;
+	public const int RelayTargetRegistryUnregisteredTarget = 10402;
+	public const int RelayTargetRegistryCouldNotUnregisterTarget = 10403;
 
-	public const int RelayWebTargetRequestingTarget = 29;
-	public const int RelayWebTargetRequestedTarget = 30;
+	public const int RelayWebTargetRequestingTarget = 10501;
+	public const int RelayWebTargetRequestedTarget = 10502;
 }
--- src/docker/Thinktecture.Relay.Connector.Docker/LoggingEventIds.cs
+++ src/docker/Thinktecture.Relay.Connector.Docker/LoggingEventIds.cs
@@ -2,10 +2,10 @@ namespace Thinktecture.Relay.Connector.Docker;
 
 internal static class LoggingEventIds
 {
-	public const int InProcFuncExecuting = 1;
-	public const int InProcFuncBodySize = 2;
-	public const int InProcFuncEcho = 3;
+	public const int InProcFuncExecuting = 10001;
+	public const int InProcFuncBodySize = 10002;
+	public const int InProcFuncEcho = 10003;
 
-	public const int ConnectorServiceStarting = 4;
-	public const int ConnectorServiceStopping = 5;
+	public const int ConnectorServiceStarting = 10101;
+	public const int ConnectorServiceStopping = 10102;
 }
