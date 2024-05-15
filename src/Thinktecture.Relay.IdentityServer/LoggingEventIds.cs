573d6e233554d72f4225baf5ac827c3c47b0a682
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2024-05-15 12:25:28 +0200

    refactor(idsrv): group logging ids

--- src/Thinktecture.Relay.IdentityServer/LoggingEventIds.cs
+++ src/Thinktecture.Relay.IdentityServer/LoggingEventIds.cs
@@ -2,9 +2,9 @@ namespace Thinktecture.Relay.IdentityServer;
 
 internal static class LoggingEventIds
 {
-	public const int RotateCertificateFileStoreRebuildingCertificateCache = 1;
-	public const int RotateCertificateFileStoreCreateCertificate = 2;
-	public const int RotateCertificateFileStoreRemoveOutdatedCertificate = 3;
-	public const int RotateCertificateFileStoreLoadCertificate = 4;
-	public const int RotateCertificateFileStoreDeleteCertificateFile = 5;
+	public const int RotateCertificateFileStoreRebuildingCertificateCache = 10001;
+	public const int RotateCertificateFileStoreCreateCertificate = 10002;
+	public const int RotateCertificateFileStoreRemoveOutdatedCertificate = 10003;
+	public const int RotateCertificateFileStoreLoadCertificate = 10004;
+	public const int RotateCertificateFileStoreDeleteCertificateFile = 10005;
 }
