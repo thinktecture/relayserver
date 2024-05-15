c6f6348d6d0cec4812a65b47afe18cc0faf3f460
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2024-05-15 12:24:18 +0200

    refactor(server): group logging ids

--- src/docker/Thinktecture.Relay.Server.Docker/LoggingEventIds.cs
+++ src/docker/Thinktecture.Relay.Server.Docker/LoggingEventIds.cs
@@ -2,9 +2,9 @@ namespace Thinktecture.Relay.Server.Docker;
 
 internal static class LoggingEventIds
 {
-	public const int DemoRequestInterceptorRemovingOutput = 1;
-	public const int DemoRequestInterceptorEnabled = 2;
+	public const int DemoRequestInterceptorRemovingOutput = 10001;
+	public const int DemoRequestInterceptorEnabled = 10002;
 
-	public const int DemoResponseInterceptorRemovingOutput = 3;
-	public const int DemoResponseInterceptorEnabled = 4;
+	public const int DemoResponseInterceptorRemovingOutput = 10101;
+	public const int DemoResponseInterceptorEnabled = 10102;
 }
