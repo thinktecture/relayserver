9b0177ccdc80e609e9d73e0c90cccd7daeb196b0
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2025-02-24 15:27:28 +0100

    chore: remove warnings from code

--- src/Thinktecture.Relay.Server/Services/DiscoveryDocumentBuilder.cs
+++ src/Thinktecture.Relay.Server/Services/DiscoveryDocumentBuilder.cs
@@ -3,7 +3,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
